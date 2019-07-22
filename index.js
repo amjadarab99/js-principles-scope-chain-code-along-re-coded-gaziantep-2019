4ff1f6eb6f7ea2230a19201253e4aad6590b85db
Amjad Arab <amjad.arab99@gmail.com>
2019-07-22 21:50:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function congratulateCustomer(){
 }
 
 function attemptTwoFavoriteCustomers(){
-   let favoriteCustomer = 'bar'
-   let favoriteCustomer = 'not bar'
+   var favoriteCustomer = 'bar'
+   var favoriteCustomer = 'not bar'
 }
