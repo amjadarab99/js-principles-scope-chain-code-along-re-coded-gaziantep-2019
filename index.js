1a8aa39f02a26da2340dfcd44c4974027db4190c
Amjad Arab <amjad.arab99@gmail.com>
2019-07-22 21:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+ bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+ bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer(){
+ leastFavoriteCustomer = 'not bob';
+}
+
+let favoriteCustomer = 'bob';
+function attemptTwoFavoriteCustomers(){
+ let favoriteCustomer = 'not bob';
+
+ throw SyntaxError(SyntaxError)
+}
